53f36ee8d6f730f72521a373442472d0ad5c310c
nazar <nazarlev20@gmail.com>
2024-06-07 21:28:54 +0300

    Task 2 of the first lecture has been completed

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Enter the 2 numbers you want to add(a+b)" << endl;
+    cout << "a:";
+    cin  >> a ;
+    cout << "b:";
+    cin >> b ;
+    cout << a << "+" << b << "=" <<a+b;
+    return 0;
+}
