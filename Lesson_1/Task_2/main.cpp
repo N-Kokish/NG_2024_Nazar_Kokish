0bdb12b20c31588c329375bc61dbbf298fb3bb3a
nazar <nazarlev20@gmail.com>
2024-06-09 19:55:15 +0300

    Changed the name of the variables

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    int a,b;
+    int number_1,number_2;
     cout << "Enter the 2 numbers you want to add(a+b)" << endl;
     cout << "a:";
-    cin  >> a ;
+    cin  >> number_1 ;
     cout << "b:";
-    cin >> b ;
-    cout << a << "+" << b << "=" <<a+b;
+    cin >> number_2 ;
+    cout << number_1 << "+" << number_2 << "=" <<number_1+number_2;
     return 0;
 }
