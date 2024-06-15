8ace1c4ba3268a447c19912df604ea0ff4108292
nazar <nazarlev20@gmail.com>
2024-06-15 17:40:40 +0300

    completed task 2 of the second lecture

---
+++ Lesson_2/Task_2/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int maximum_numbers=0;
+    int array_numbers[20];
+    for(int number_index=0;number_index<20;number_index++){
+        cout << "Enter value: ";
+        cin >>array_numbers[number_index];
+        if (maximum_numbers<array_numbers[number_index]){
+            maximum_numbers =array_numbers[number_index];
+            //maximum_account_index=number_index+1;
+        }
+        if (array_numbers[number_index]==0){
+            break;
+        }
+
+    }
+    for (int numeric : array_numbers) {
+        if (numeric==0){
+            break;
+        }
+        for (int space=0;space<(maximum_numbers-numeric)/2;space++){
+            cout<<" ";
+        }
+        for (int asterisks=0;asterisks<numeric;asterisks++){
+            cout<<"*";
+        }
+        cout <<   endl;
+    }
+    return 0;
+}
