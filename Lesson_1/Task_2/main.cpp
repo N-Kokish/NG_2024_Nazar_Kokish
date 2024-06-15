f9f24a9d787d4d253c5593ae14ddf821e16b2278
nazar <nazarlev20@gmail.com>
2024-06-15 14:02:40 +0300

    completed task 3 of the second lecture

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
     int number_1,number_2;
-    cout << "Enter the 2 numbers you want to add(a+b)" << endl;
-    cout << "a:";
+    cout << "Enter the 2 numbers you want to add(number_1+number_2)" << endl;
+    cout << "enter 1 app :";
     cin  >> number_1 ;
-    cout << "b:";
+    cout << "enter 2 application : ";
     cin >> number_2 ;
     cout << number_1 << "+" << number_2 << "=" <<number_1+number_2;
     return 0;
