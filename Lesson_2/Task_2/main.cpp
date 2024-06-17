16d1d9f3c9492cbbff78f25143ee96ef245018ce
nazar <nazarlev20@gmail.com>
2024-06-18 01:41:28 +0300

    improved the request to the user

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -6,8 +6,9 @@ int main()
 {
     int maximum_numbers=0;
     int array_numbers[20];
+    cout << "You are now setting up your \"unique\" shape, now you will keep the size of each floor shape,\n in order to complete the entered data, enter 0."<<endl;
     for(int number_index=0;number_index<20;number_index++){
-        cout << "Enter value: ";
+        cout << "Enter the size of the floor number "<<number_index+1<<" : ";
         cin >>array_numbers[number_index];
         if (maximum_numbers<array_numbers[number_index]){
             maximum_numbers =array_numbers[number_index] ;
