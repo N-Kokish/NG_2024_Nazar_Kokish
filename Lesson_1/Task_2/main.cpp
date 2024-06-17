b81f87ed29ab8f1edf88bd00ebb823fc96e9ff16
nazar <nazarlev20@gmail.com>
2024-06-18 01:19:43 +0300

    improved 2 tasks of the first lecture

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
     int number_1,number_2;
-    cout << "Enter the 2 numbers you want to add(number_1+number_2)" << endl;
-    cout << "enter 1 app :";
+    cout << "Enter the 2 numbers you want to add(number1+number2)" << endl;
+    cout << "enter number 1 :";
     cin  >> number_1 ;
-    cout << "enter 2 application : ";
+    cout << "enter number 2 : ";
     cin >> number_2 ;
     cout << number_1 << "+" << number_2 << "=" <<number_1+number_2;
     return 0;
