66d134f2671bc78feea6f9df3a4757296ba70ab5
nazar <nazarlev20@gmail.com>
2024-06-18 12:45:40 +0300

    simplified the code of task 3 of the second lecture

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int maximum_numbers=0;
     int array_numbers[5];
-    \
+    //int maximum_account_index;
     for(int number_index=0;number_index<5;number_index++){
         cout << "Enter amount in "<<number_index+1<<" row:  " ;
         cin >>array_numbers[number_index];
@@ -15,31 +15,18 @@ int main()
             //maximum_account_index=number_index+1;
         }
     }
-    char figure[maximum_numbers][5];
-    for(int i =0;i<maximum_numbers;i++){
-        for(int j=0;j<5;j++){
-            figure[i][j]='o';
-        }
-    }
-
-    for(int j=0;j<5;j++){
-        for(int i =0;i<array_numbers[j];i++){
-            figure[i][j]='*';
+    for(int number_index=0;number_index<maximum_numbers;number_index++){
+        for(int counter=0;counter<5;counter++){
+            if (array_numbers[counter]>0){
+                cout<<"*";
+            }else{
+                cout<<" ";
             }
-        }
-
-
-
-
-
-    for(int i =0;i<maximum_numbers;i++){
-        for(int j=0;j<5;j++){
-            cout<<figure[i][j];
+            array_numbers[counter]=array_numbers[counter]-1;
         }
         cout<<endl;
     }
-    cout <<maximum_numbers;
 
-    cout << "Hello World!" << endl;
+
     return 0;
 }
